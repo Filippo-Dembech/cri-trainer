78da86cb381d7c7cfa49cecaf229671193d50ad0
Filippo Dembech <Filippo.dembech@gmail.com>
2023-06-02 20:28:06 +0200

    refactoring: document Skill class in app.js

--- app.js
+++ app.js
@@ -1,34 +1,51 @@
+// @ts-check
+
 class Skill {
 
-    constructor (title, steps) {
-        this.title = title;
-        this.steps = steps;
+    /**
+     * Title of the `Skill`
+     * @type {string} 
+     */
+    #title;
+
+    /**
+     * Array of strings representing the steps of the `Skill`
+     * @type {Array<string>}
+     */
+    #steps;
+
+    /**
+     * @constructor
+     * @param {string} title - Title of the `Skill`
+     * @param {Array<string>} [steps] - Array of strings defining the steps of the `Skill` 
+     */
+    constructor (title, steps=[]) {
+        this.#title = title;
+        this.#steps = steps;
     }
 
-    getTitle() { return this.title }
-    getSteps() { return this.steps }
+    get title() { return this.#title }
+    get steps() { return this.#steps }
+
+    set title(title) { this.#title = title }
+    set steps(steps) { this.#steps = steps }
+
 }
 
 
-collare = new Skill(
-    "Collare Cervicale",
-    [
-        "Fai questo",
-        "Fai Quello", 
-        "di qua",
-        "di là",
-        "di giù",
-        "di sù"
-    ]
-)
-
-spinale = new Skill("Spinale", [])
+const collare = new Skill("Collare Cervicale")
 
 
-const skillSelector = document.querySelector("#skill");
+const spinale = new Skill("Spinale", [])
+
+
+const skillSelector = document.getElementById("#skill");
 const newSkill = document.createElement("option");
 const skillSpinale = document.createElement("option");
 newSkill.innerHTML = collare.title;
 skillSpinale.innerHTML= spinale.title;
-skillSelector.appendChild(newSkill);
-skillSelector.appendChild(skillSpinale)
+
+if (skillSelector) {
+    skillSelector.appendChild(newSkill);
+    skillSelector.appendChild(skillSpinale)
+}
