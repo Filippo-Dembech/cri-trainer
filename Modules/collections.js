0a8c582126ea933221eb93cde61455184ce21513
Filippo Dembech <Filippo.dembech@gmail.com>
2023-06-08 19:40:38 +0200

    bugfix: app.js fixed an html DOM element selection
    document.getElementById() method used with pound sign '#'

--- Modules/collections.js
+++ Modules/collections.js
@@ -33,5 +33,3 @@ class Skill {
 
 }
 
-
-export { Skill }
--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 // @ts-check
-import { Skill } from "./Modules/collections.js"
-
 
 const collare = new Skill("Collare Cervicale")
 
@@ -9,12 +7,13 @@ const collare = new Skill("Collare Cervicale")
 const spinale = new Skill("Spinale", [])
 
 
-const skillSelector = document.getElementById("#skill");
+const skillSelector = document.getElementById("skills");
 const newSkill = document.createElement("option");
 const skillSpinale = document.createElement("option");
 newSkill.innerHTML = collare.title;
 skillSpinale.innerHTML= spinale.title;
 
+console.log(skillSelector);
 if (skillSelector) {
     skillSelector.appendChild(newSkill);
     skillSelector.appendChild(skillSpinale)
