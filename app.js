d85f638074d29fb1c3b77826600690ee0a9a1101
Filippo Dembech <Filippo.dembech@gmail.com>
2023-06-12 17:07:34 +0200

    add: go away animation when option is choosen

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-// @ts-check
+const selectionButton = document.getElementById("selection-button");
+
 
 const collare = new Skill("Collare Cervicale")
-
-
-
 const spinale = new Skill("Spinale", [])
 
 
@@ -13,8 +11,22 @@ const skillSpinale = document.createElement("option");
 newSkill.innerHTML = collare.title;
 skillSpinale.innerHTML= spinale.title;
 
-console.log(skillSelector);
-if (skillSelector) {
-    skillSelector.appendChild(newSkill);
-    skillSelector.appendChild(skillSpinale)
-}
+skillSelector.appendChild(newSkill);
+skillSelector.appendChild(skillSpinale)
+
+selectionButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    const frontSelection = document.getElementById("front-selection");
+    frontSelection.style.animation = "go-away 3s";
+})
+
+
+
+
+
+
+
+
+
+
+
